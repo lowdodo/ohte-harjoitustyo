c5d951b9c302c3a2d57526c6d4c85a74747076bf
lowdodo <lowdodo@ad.helsinki.fi>
2024-04-16 23:31:29 +0300

    updated button.py

--- src/button.py
+++ src/button.py
@@ -4,16 +4,19 @@ class Button():
         self.rect = self.image.get_rect(center=pos)
         self.text = text
         self.font = font
-        self.render_text()
 
     def render_text(self):
         if self.text:
             text_surface = self.font.render(self.text, True, (0, 0, 0))
-            text_rect = text_surface.get_rect(center= self.rect.center) 
-            self.image.blit(text_surface, text_rect)
+            text_rect = text_surface.get_rect(center=self.rect.center)
+            
+            return text_surface, text_rect
 
     def update(self, screen):
         screen.blit(self.image, self.rect)
+        if self.text:
+            text_surface, text_rect = self.render_text()
+            screen.blit(text_surface, text_rect)
 
     def check_for_input(self, position):
         return self.rect.collidepoint(position)
