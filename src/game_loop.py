1a39c24610aeb0e8a4ac13b3f2dd5245c543aa41
lowdodo <lowdodo@ad.helsinki.fi>
2024-04-16 23:59:50 +0300

    updated game_loop to work with ui

--- src/game_loop.py
+++ src/game_loop.py
@@ -8,33 +8,31 @@ class GameLoop:
         self._level = level
 
     def start(self):
-        running = True
         while True:
-            if self._handle_events() == False:
+            if self._handle_events() is False:
                 break
 
-            running = self._handle_events()
+            self._handle_events()
             self._level.update()
             self._clock.tick(60)
-            
+
     def _handle_events(self):
         for event in self._event_queue.get():
             if event.type == pygame.QUIT:
                 return False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    self._level.move_child(x = -6, y = 0)
+                    self._level.move_child(x=-6, y=0)
                 if event.key == pygame.K_RIGHT:
-                    self._level.move_child(x = 6, y = 0)
+                    self._level.move_child(x=6, y=0)
                 if event.key == pygame.K_UP:
-                    self._level.move_child(x = 0, y = -6)
+                    self._level.move_child(x=0, y=-6)
                 if event.key == pygame.K_DOWN:
                     pass  # do we have to crouch?
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    self._level.move_petal(x = event.pos[0])
-                    self._level.move_petal(y = event.pos[1])
+                    self._level.move_petal(x=event.pos[0])
+                    self._level.move_petal(y=event.pos[1])
 
         self._level.child_sprite.update()
         self._level.petal_sprite.update()
         return True
-    
