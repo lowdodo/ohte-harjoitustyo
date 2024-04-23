a991f9af00dea1176bb208c4362c8ced93151d35
lowdodo <lowdodo@ad.helsinki.fi>
2024-04-23 22:50:42 +0300

    added docstring

--- src/imagepath.py
+++ src/imagepath.py
@@ -5,6 +5,11 @@ dirname = os.path.dirname(__file__)
 
 
 def imagepath(filename):
+    """Brings png-file for other code to use.
+    
+    Args:
+        filename: name of the file that is searched.
+    """
     return pygame.image.load(
         os.path.join(dirname, "assets", filename)
     )
