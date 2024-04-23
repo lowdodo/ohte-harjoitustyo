a084964a839515d9502597a90498e48e9d347580
lowdodo <lowdodo@ad.helsinki.fi>
2024-04-23 23:48:25 +0300

    created ui path

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -3,24 +3,27 @@ from ui.choose_player import LoginView
 from ui.level_view import LevelView
 from ui.menu import MenuView
 from ui.new_player import NewPlayerView
-from level import Level
 
 
 class UI:
-    def __init__(self, screen, current_level, handle_play, handle_quit) -> None:
+    def __init__(self, screen, handle_play, handle_quit, event_queue, current_level) -> None:
         self._screen = screen
         self._current_view = None
         self._current_level = current_level
         self._handle_play = handle_play
         self._handle_quit = handle_quit
+        self._event_queue = event_queue
 
     def start(self):
 
         # these are for manual testing
-        self._show_menu_view()
-        # level_number = self._current_level
-        # self._show_level_view(level_number)
+        print("menossa menuview")
+        if not self._current_level:
+            self._show_menu_view()
+        else:
+            self._show_level_view()
         # self._show_choose_player()
+        # self._show_new_player_view()
 
     def _hide_current_view(self):
         if self._current_view:
@@ -41,28 +44,33 @@ class UI:
         self._hide_current_view()
         self._current_view = NewPlayerView(
             self._screen,
-            self._show_menu_view
-        )
-
-        self._current_view.render()
-
-    def _show_menu_view(self):
-        self._hide_current_view()
-        self._current_view = MenuView(
-            self._screen,
             self._handle_play,
             self._handle_quit
         )
 
         self._current_view.render()
 
-    def _show_level_view(self, level_number):
+    def _show_menu_view(self):
         self._hide_current_view()
-        level_instance = Level(self._screen, pygame.event, level_number)
+        print("nyt showmenuview.ui")
+        self._current_view = MenuView(
+            self._screen,
+            self._handle_play,
+            self._handle_quit,
+            self._event_queue,
+            self._show_level_view
+        )
+        self._current_view.render()
+
+    def _show_level_view(self):
+        print("päästiin showlevel")
+        self._hide_current_view()
         self._current_view = LevelView(
             self._screen,
-            self._current_level,
-            level_instance
+            self._handle_play,
+            self._handle_quit,
+            self._event_queue,
+            self._current_level
         )
 
         self._current_view.render()
