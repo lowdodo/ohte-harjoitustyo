8b2ce02716649c7839f3dd150cc799982d9b0f55
lowdodo <lowdodo@ad.helsinki.fi>
2024-04-23 23:52:09 +0300

    added docsting

--- src/index.py
+++ src/index.py
@@ -6,29 +6,33 @@ from game_loop import GameLoop
 from clock import Clock
 from event_queue import EventQueue
 
-
 def main():
+    """Entry point to the game.
+    
+    Initialazes Pygame, sets up screen for the game, game loop and gandles exiting
+    
+    Attributes: 
+        Pygame engine: Handles game window creation and event handling.
+        EventQueue: Queue for handling game events.
+        Level: Represents the current level on the game.
+        UI: The user interface and interaction with the game.
+        GameLoop: Controls the main game loop and updates the game.
+ 
+    """
     pygame.init()
     screen = pygame.display.set_mode((1000, 600))
-    pygame.display.set_caption("Main menu")
+    pygame.display.set_caption("Main menus")
     event_queue = EventQueue()
     clock = Clock()
-    level = Level(screen, event_queue, level_number=0)
-    ui = UI(screen, level.current_level, handle_play, handle_quit)
+    level = Level(screen, event_queue, current_level=None)
+    ui = UI(screen, level.handle_play, level.handle_quit,
+            event_queue, level.current_level)
     ui.start()
 
     game_loop = GameLoop(event_queue, clock, level)
     game_loop.start()
 
     pygame.quit()
-    sys.exit()
-
-
-def handle_play(level):
-    level.run_prescreen()
-
-
-def handle_quit():
     sys.exit()
 
 
