e884a95bbf26f32a681e6fee66071ad56c345ed4
lowdodo <lowdodo@ad.helsinki.fi>
2024-04-30 22:12:58 +0300

    made config

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+dirname = os.path.dirname(__file__)
+
+try:
+    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
+except FileNotFoundError:
+    pass
+
+GAME_FILENAME = os.getenv("GAME_FILENAME") or "game.csv"
+GAME_FILE_PATH = os.path.join(dirname, "..", "data", GAME_FILENAME)
