3871865d1182f52ecda9e5151df64a5709f98271
lowdodo <lowdodo@ad.helsinki.fi>
2024-04-30 22:14:10 +0300

    added init database

---
+++ src/initialize_database.py
@@ -0,0 +1,20 @@
+from database_connection import get_database_connection
+
+def drop_table(connection):
+    cursor = connection.cursor()
+    cursor.execute("DROP TABLE if exists player;")
+
+def create_player_table(connection):
+    cursor = connection.cursor()
+    cursor.execute("""CREATE TABLE player
+                   (name TEXT);""")
+    connection.commit()
+
+
+def initialize_database():
+    connection = get_database_connection()
+    drop_table(connection)
+    create_player_table(connection)
+
+if __name__ == "__main__":
+    initialize_database()
