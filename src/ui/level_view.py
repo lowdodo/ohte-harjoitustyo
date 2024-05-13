c524b9f0954d001f2d7e9bfce87f6b2b047771e8
lowdodo <lowdodo@ad.helsinki.fi>
2024-05-13 08:07:50 +0300

    new level

--- src/ui/level_view.py
+++ src/ui/level_view.py
@@ -7,7 +7,12 @@ from button import Button
 from imagepath import imagepath
 from sprites.child import Child
 from sprites.petal import Petal
+from repositories.player_repository import PlayerRepository
+from database_connection import get_database_connection
+from initialize_database import initialize_database
+from players.player import Player
 import math
+
 
 
 class LevelView:
@@ -31,6 +36,9 @@ class LevelView:
         self._petal_moves = False
         self.player_name = ""
 
+        self._player_repository = PlayerRepository(get_database_connection())
+        self._player = None
+
     def handle_event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -53,7 +61,7 @@ class LevelView:
         self._screen.blit(text, text_rect)
         self.child_sprite.update()
         self._screen.blit(self.child_sprite.image, self.child_sprite.rect)
-
+        pygame.draw.circle(self._screen, color=(255, 255, 255), center=(100, 80), radius=30)
         pygame.display.flip()
 
         play_next = Button(image=imagepath("nextbutton.png"),
@@ -74,13 +82,11 @@ class LevelView:
                             self.child_sprite.rect.x += 1
                             self._screen.fill((0, 0, 0))
                             self._screen.blit(text, text_rect)
+                            pygame.draw.circle(self._screen, color=(255, 255, 255), center=(100, 80), radius=30)
                             self._screen.blit(
                                 self.child_sprite.image, self.child_sprite.rect)
                             pygame.display.update()
-                        print("päästiin drawpre loopin loppuun")
-                        # Start level1
                         self._handle_play()
-                        print("nyt olemme prescreen whilen lopsssa")
                         running = False
 
     def draw_level(self):
@@ -102,7 +108,7 @@ class LevelView:
             # todo: when petal is clicked, it takes you to new level
             running = True
             while running:
-                for event in pygame.event.get():
+                for event in self._event_queue.get():
                     mouse_pos = pygame.mouse.get_pos()
                     if event.type == pygame.QUIT:
                         pygame.quit()
@@ -125,15 +131,11 @@ class LevelView:
                             self._handle_play()
                             running = False
 
-                # moving smoothly
                 if self._left:
                     self.child_sprite.rect.x -= 6
                 if self._right:
                     self.child_sprite.rect.x += 6
-                # if up:
-                #     self.child_sprite.rect.y -= 2
 
-                # not going past the screen:
                 if self.child_sprite.rect.left < 0:
                     self.child_sprite.rect.left = 0
                 elif self.child_sprite.rect.right > 1000:
@@ -162,7 +164,6 @@ class LevelView:
             text1_rect = text1.get_rect(center= (500, 30))
             self._screen.blit(text1, text1_rect)
 
-            
             pygame.draw.circle(self._screen, (200, 200, 200), (500, 300), 70)
 
             # correct positions for petals:
@@ -200,24 +201,35 @@ class LevelView:
 
             pygame.display.update()
 
+            last_clicked = None
+
             running = True
             while running:
-                for event in pygame.event.get():
+                for event in self._event_queue.get():
                     mouse_pos = pygame.mouse.get_pos()
                     if event.type == pygame.QUIT:
                         pygame.quit()
                         quit()
                     if event.type == pygame.MOUSEBUTTONDOWN:
-                        for petal in petals:
-                            if petal.rect.collidepoint(mouse_pos):
-                                print("saatiin klikki")
-                                petal.click()
-                                petal_drag = True
+                        if not petal_drag:
+                            clicked_petal = None
+                            for petal in petals:
+                                if petal.rect.collidepoint(mouse_pos) and not petal.clicked:
+                                    clicked_petal = petal
+                                    break
+                            if clicked_petal:
+                                if not last_clicked or last_clicked == clicked_petal:
+                                    print("saatiin klikki")
+                                    clicked_petal.click()
+                                    last_clicked = clicked_petal
+                                    petal_drag = True
 
                     elif event.type == pygame.MOUSEBUTTONUP:
-                        for petal in petals:
-                            petal.release()
+                        if last_clicked:
+                            last_clicked.release()
+                            last_clicked = None
                             petal_drag = False
+
                     elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                         for petal in petals:
                             if petal.clicked:
@@ -278,26 +290,248 @@ class LevelView:
             text_rect = text.get_rect(center=(640, 260))
             self._screen.blit(text, text_rect)
 
-            name = pygame.font.SysFont(None, 30).render(
-                self.player_name, True, (255, 255, 255))
-            name_rect = name.get_rect(center=(640, 320))
-            pygame.draw.rect(self._screen, (255, 255, 255), name_rect, 2)
-            self._screen.blit(name, name_rect)
+            play_next = Button(image=imagepath("nextbutton.png"),
+                            pos=(900, 350), text=None, font=None)
+
+            play_next.update(self._screen)
+            pygame.display.flip()
+            # name = pygame.font.SysFont(None, 30).render(
+            #     self.player_name, True, (255, 255, 255))
+            # name_rect = name.get_rect(center=(640, 320))
+            # pygame.draw.rect(self._screen, (255, 255, 255), name_rect, 2)
+            # self._screen.blit(name, name_rect)???
+
+            running = True
+            while running:
+                for event in self._event_queue.get():
+                    mouse_pos = pygame.mouse.get_pos()
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if play_next.check_for_input(mouse_pos):
+                            while self.child_sprite.rect.x <= 1000:
+                                self.child_sprite.rect.x += 1
+                                self._screen.fill((0, 0, 0))
+                                self._screen.blit(text, text_rect)
+                                self._screen.blit(
+                                    self.child_sprite.image, self.child_sprite.rect)
+                                pygame.display.update()
+                            print("päästiin lvl3 loopin loppuun")
+                            self._handle_play()
+                            running = False
 
             pygame.display.flip()
 # name picking
         if self.current_level == 4:
-            pass
-            # todo name picking
+            pygame.display.set_caption("level4")
+            self._screen.fill((0, 0, 0))
+            clock = pygame.time.Clock()
+
+            confirm_button = Button(image=imagepath("nextbutton.png"),
+                            pos=(900, 550), text=None, font=None)
+            
+            prev_button = Button(image=imagepath("previousbutton.png"),
+                            pos = (840, 550), text= None, font=None)
+            base_font = pygame.font.Font(None, 32)
+            player_name = ""
+            input_rect = pygame.Rect(200, 200, 140, 32)
+            color_active = pygame.Color('lightskyblue3') 
+            color_passive = pygame.Color((245, 245, 240)) 
+            color = color_passive 
+
+            active = False
+            initialize_database()
+            user_text = ""
+            text = pygame.font.SysFont(None, 25).render(
+                "Now then, what is your name dear?", True, (255, 255, 255))
+            text_rect = text.get_rect(center=(500, 10))
+            self._screen.blit(text, text_rect)
+            confirm_button.update(self._screen)
+            prev_button.update(self._screen)
+            pygame.display.flip()
+            # Confirming name
+            confirmed = False
+            confirmed_twice = False
+            running = True
+            while running:
+                for event in self._event_queue.get():
+                    mouse_pos = pygame.mouse.get_pos()
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        mouse_pos = pygame.mouse.get_pos()
+                        if input_rect.collidepoint(mouse_pos):
+                            active = True
+                        else:
+                            active = False
+
+                        if confirm_button.rect.collidepoint(mouse_pos) and not confirmed:
+                            confirmed = True
+                            self._screen.fill((0,0,0))
+                            text1 = pygame.font.SysFont(None, 25).render(f"{user_text}, really?", True, (255, 255, 255))
+                            self._screen.blit(text1, (100, 50))
+                            confirm_button.update(self._screen)
+                            prev_button.update(self._screen)
+                            pygame.display.flip()
+                        
+                        elif confirm_button.rect.collidepoint(mouse_pos) and confirmed:
+                            self._screen.fill((0,0,0))
+                            confirm_button.update(self._screen)
+                            prev_button.update(self._screen)
+                            if confirmed_twice:
+                                player_name = user_text
+                                player = self._player_repository.name_the_player(player_name)
+                                self._handle_play()
+                            else:
+                                confirmed_twice = True
+                                text2 = pygame.font.SysFont(None, 25).render(f"{user_text}, reeeeeeally? My dear, who named you!", True, (255, 255, 255))
+                                self._screen.blit(text2, (120, 80))
+                                confirm_button.update(self._screen)
+                                prev_button.update(self._screen)
+                                pygame.display.flip()
+
+                        elif prev_button.rect.collidepoint(mouse_pos):
+                            confirmed = False
+                            confirmed_twice = False
+                            self._screen.fill((0,0,0))                            
+                            text2 = pygame.font.SysFont(None, 25).render("Well what then? Silly thing", True, (255, 255, 255))
+                            self._screen.blit(text2, (100, 50))
+                            confirm_button.update(self._screen)
+                            prev_button.update(self._screen)
+                
+
+
+                    if event.type == pygame.KEYDOWN: #for typing, looked example from here: https://www.geeksforgeeks.org/how-to-create-a-text-input-box-with-pygame/?ref=next_article
+                        if event.key == pygame.K_BACKSPACE:
+                            user_text = user_text[:-1] 
+                        else:
+                            user_text += event.unicode
+                if active:
+                    color = color_active
+                else:
+                    color = color_passive
+                pygame.draw.rect(self._screen, color, input_rect)
+                text_surface = base_font.render(user_text, True, (255, 255, 255))
+                self._screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
+                input_rect.w = max(100, text_surface.get_width()+10)
+                pygame.display.flip()
+                clock.tick(60)      
+
+
 
         if self.current_level == 5:
-            pass
+            pygame.display.set_caption("level5")
+            self._screen.fill((0, 0, 0))
+            clock = pygame.time.Clock()
+
+            self._screen.blit(self.child_sprite.image, self.child_sprite.rect)
+            self._screen.blit(self.petal_sprite.image, self.petal_sprite.rect)
+
+            petal_button = Button(image=imagepath("petal.png"),
+                                  pos=((750, 510)), text=None, font=None
+                                  )
+            petal_button.update(self._screen)
+            pygame.display.update()
+
+            running = True
+            while running:
+                for event in self._event_queue.get():
+                    mouse_pos = pygame.mouse.get_pos()
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_LEFT:
+                            self._left = True
+                        elif event.key == pygame.K_RIGHT:
+                            self._right = True
+
+                    if event.type == pygame.KEYUP:
+                        if event.key == pygame.K_LEFT:
+                            self._left = False
+                        elif event.key == pygame.K_RIGHT:
+                            self._right = False
+
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if petal_button.check_for_input(mouse_pos):
+                            print("klikattiin petalia")
+                            self._handle_play()
+                            running = False
+
+                # moving smoothly
+                if self._left:
+                    self.child_sprite.rect.x -= 6
+                if self._right:
+                    self.child_sprite.rect.x += 6
+                # if up:
+                #     self.child_sprite.rect.y -= 2
+
+                # not going past the screen:
+                if self.child_sprite.rect.left < 0:
+                    self.child_sprite.rect.left = 0
+                elif self.child_sprite.rect.right > 1000:
+                    self.child_sprite.rect.right = 1000
+
+                self._screen.fill((0, 0, 0))
+                self._screen.blit(self.child_sprite.image,
+                                  self.child_sprite.rect)
+                self._screen.blit(self.petal_sprite.image,
+                                  self.petal_sprite.rect)
+                pygame.display.update()
+                clock.tick(60)
+
             # todo interaction with name, transition to further away
 
 # 1st riddle
         if self.current_level == 6:
-            # todo first riddle
-            pass
+            self._screen.fill((0,0,0))
+            text = pygame.font.SysFont(None, 35).render(
+                "Moon is just a mirror for Sun to admire herself", True, (255, 255, 255))
+            text_rect = text.get_rect(center=(500, 80))
+            self._screen.blit(text, text_rect)
+
+            mirror_button = Button(image=imagepath("mirror.png"),
+                                  pos=((750, 510)), text=None, font=None
+                                  )
+            mirror_button.update(self._screen)
+            mirror_broken = imagepath("brokenmirror.png")
+            pygame.display.update()
+
+            running = True
+            while running:
+                for event in self._event_queue.get():
+                    mouse_pos = pygame.mouse.get_pos()
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if mirror_button.rect.collidepoint(mouse_pos):
+                            self._screen.fill((0,0,0))
+                            mirror_broken.get_rect(750, 510)
+                            text1 = pygame.font.SysFont(None, 35).render(
+                                "Oh. You broke it.", True, (255, 255, 255))
+                            text1_rect = text.get_rect(center=(500, 80))
+                            self._screen.blit(text1, text1_rect)
+                            #todo: make delayed transition to next level
+                            self._handle_play()
+
+
+                            
+
+        if self.current_level == 7:
+            self._screen.fill((0,0,0))
+            text = pygame.font.SysFont(None, 35).render(
+                "You should run now, for she will be bitter and angry", True, (255, 255, 255))
+            text_rect = text.get_rect(center=(500, 80))
+            
+            text1 = pygame.font.SysFont(None, 35).render(
+                "Raindrops are her tears, falling all the way to our world ", True, (255, 255, 255))
+            text1_rect = text1.get_rect(center=(500, 80))
+            self._screen.blit(text, text_rect)
+            self._screen.blit(text1, text1_rect)
+
+            
+
+
 
     def render(self):
         if self.current_level == 0:
@@ -305,9 +539,3 @@ class LevelView:
         else:
             self.draw_level()
 
-    # def run(self):
-    #     while True:
-    #         self.handle_events()
-    #         self.draw_prescreen()
-    #         self.render()
-    #         pygame.display.flip()
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,10 +1,8 @@
 """
 Module for the main ui side.
 """
-from ui.choose_player import LoginView
 from ui.level_view import LevelView
 from ui.menu import MenuView
-from ui.new_player import NewPlayerView
 
 
 class UI:
@@ -30,40 +28,16 @@ class UI:
         self._event_queue = event_queue
 
     def start(self):
-
-        # these are for manual testing
-        print("menossa menuview")
         if not self._current_level:
             self._show_menu_view()
         else:
             self._show_level_view()
-        # self._show_choose_player()
-        # self._show_new_player_view()
 
     def _hide_current_view(self):
         if self._current_view:
             self._current_view.destroy()
 
         self._current_view = None
-
-    def _show_choose_player(self):
-        self._hide_current_view()
-        self._current_view = LoginView(
-            self._screen,
-            self._show_new_player_view
-        )
-
-        self._current_view.render()
-
-    def _show_new_player_view(self):
-        self._hide_current_view()
-        self._current_view = NewPlayerView(
-            self._screen,
-            self._handle_play,
-            self._handle_quit
-        )
-
-        self._current_view.render()
 
     def _show_menu_view(self):
         self._hide_current_view()
